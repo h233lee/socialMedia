a17fbc73a6581f03b1b6fdb1d3f18e49dc3bc472
Andrew <hokyoon.lee94@gmail.com>
2020-08-02 21:45:15 -0400

    another commit

--- server/server.js
+++ server/server.js
@@ -3,16 +3,16 @@ const connectDB = require('./config/db');
 
 const app = express();
 
-
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 // Connect DB
-connectDB()
+connectDB();
 
 // Init Middleware
-app.use(express.json({
-    extended: false
-}));
-
+app.use(
+  express.json({
+    extended: false,
+  })
+);
 
 app.get('/', (req, res) => res.send('API is running'));
 
@@ -21,6 +21,5 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
-
 
 app.listen(PORT, () => console.log(`Server up on port ${PORT}`));
