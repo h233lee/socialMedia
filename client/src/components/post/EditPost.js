567be155b0f8ced55896efad51451853c7085327
Andrew <hokyoon.lee94@gmail.com>
2020-08-07 10:29:46 -0400

    fixed the editPost input bar

--- client/src/components/post/EditPost.js
+++ client/src/components/post/EditPost.js
@@ -16,6 +16,9 @@ const EditPost = ({
 
   useEffect(() => {
     getPost(match.params.id);
+    if (!loading) {
+      setText(post.text);
+    }
   }, [loading, getPost, match.params.id]);
 
   const onSubmit = (e) => {
